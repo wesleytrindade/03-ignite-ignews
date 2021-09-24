97209d20b5a6b21ab50861185c2dc34704f2ef4b
Wesley Trindade Guarnieri - WGR <wesley.guarnieri@iteris.com.br>
2021-09-24 17:31:35 -0300

    implementação da tela do admin

---
+++ src/pages/admin.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+export default function Admin() {
+
+    return (
+
+        <>
+            <iframe id="ytplayer" width="100%" height="900"
+                src="https://www.youtube.com/embed/QB7ACr7pUuE?controls=0?autoplay=1?muted=1"
+                allow="autoplay"
+                frameBorder="0" allowFullScreen></iframe>
+
+            <Head>
+                <title>Rickroll | ig.news</title>
+            </Head>
+        </>
+
+    )
+
+
+}
